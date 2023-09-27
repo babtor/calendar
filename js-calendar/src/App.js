82e0467cadaf70c2d809ad46c5469aef14fe07fd
babtor <lucaswikstrom96@gmail.com>
2023-09-27 13:23:31 +0200

    Implemented dynamic routing for each date, linking to blank page

--- js-calendar/src/App.js
+++ js-calendar/src/App.js
@@ -1,13 +1,30 @@
 import "./App.css";
-import Calendar from "./components/date-box";
+import Calendar from "./components/Calendar";
+import DatePage from "./components/DatePage";
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Calendar />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Calendar />} />
+          <Route path="/date/:dateId" element={<DatePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+<div className="App">
+  <Calendar />
+</div>;
 
 export default App;
