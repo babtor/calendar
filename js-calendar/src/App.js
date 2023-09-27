306fce2562666e9f113ad1e157e1aa90c4424f97
babtor <lucaswikstrom96@gmail.com>
2023-09-27 10:28:28 +0200

    Implemented working calendar with date-fns as logic for dates

--- js-calendar/src/App.js
+++ js-calendar/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Calendar from "./components/date-box";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
 }
 
 export default App;
