363a4d685d162a00b0538fe71dda66b1ed49d6ae
babtor <lucaswikstrom96@gmail.com>
2023-10-04 10:42:01 +0200

    Implemented custom hook for localstorage of alarms

--- js-calendar/src/components/DatePage.js
+++ js-calendar/src/components/DatePage.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+// Local storage hook
+function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
 
 function DatePage() {
   const { dateId } = useParams();
+  const [alarm, setAlarm] = useLocalStorage(`Alarm-${dateId}`, "");
 
   return (
-    <div>
-      <h2>Blank Date Page</h2>
-      <p>Date: {dateId}</p>
-      {}
-    </div>
+    <>
+      <div>
+        <h2>Blank Date Page</h2>
+        <p>Date: {dateId}</p>
+        <p>{alarm}</p>
+      </div>
+      <form>
+        <input value={alarm} onChange={(e) => setAlarm(e.target.value)} />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </>
   );
 }
 
